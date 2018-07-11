753d7f86a337880a04156abb390b7699da62853d
Kepner Wu <kep-w@users.noreply.github.com>
2018-07-11 12:03:44 +0800

    多线程创建

--- blog/tornadoserver.py
+++ blog/tornadoserver.py
@@ -1,9 +1,13 @@
+import tornado.netutil
+import tornado.process
+
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.options import define, options, parse_config_file
 
 from blog.app.myapp import IndexHandler, LoginHandler, BlogHandler, RegisterHandler, CheckHandler, LoginModule, \
     BlogModule, RegisterModule, MyApplication, PublishHandler
+
 
 define('port', type=int, default=8888)
 parse_config_file('../config/config')
@@ -20,8 +24,9 @@ app = MyApplication(hs=[('/', IndexHandler),
                     um={'loginmodule': LoginModule,
                         'blogmodule': BlogModule,
                         'registermodule': RegisterModule})
-
+# 创建多线程解决单线程问题
+sockets = tornado.netutil.bind_sockets(options.port)
+tornado.process.fork_processes(4)
 server = HTTPServer(app)
-server.listen(options.port)
-IOLoop.current().start()
-
+server.add_sockets(sockets)
+IOLoop.instance().start()
